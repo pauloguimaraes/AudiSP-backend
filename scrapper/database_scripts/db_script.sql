0f82308212e56311ab367720129f28b8555da1ca
totapo <matheusraveneziani@gmail.com>
2018-11-20 17:20:03 -0200

    corrigindo script e carregamento do banco

--- scrapper/database_scripts/db_script.sql
+++ scrapper/database_scripts/db_script.sql
@@ -23,7 +23,7 @@ CREATE UNIQUE INDEX un_url ON publicacao(url_devcolab);
 CREATE TABLE publicacao_limpa (
     fk_id_publicacao INT NOT NULL primary key
     ,texto TEXT NOT NULL
-    ,processada INT NOT NULL DEFAULT (0)
+    ,processada INT NOT NULL DEFAULT 0
     ,FOREIGN KEY(fk_id_publicacao) REFERENCES publicacao(id)
 );
 
